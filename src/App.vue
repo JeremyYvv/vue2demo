877958afb2f9d1a718f8934628e2e06e7cfba2d4
Jeremy <2836933327@qq.com>
2021-03-02 00:11:05 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <div>
+      {{ info }}
+    </div>
+    <hello-world :editable="editable" :info.sync="info"></hello-world>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      editable: 'edit',
+      info: 'my demo'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <input v-model="innerInfo"/>
+    <button @click="onClick">
+      {{ editable }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['editable', 'info'],
+  methods: {
+    onClick: function() {
+      this.$emit('update:info', this.innerInfo)
+    }
+  },
+  data() {
+    return {
+      innerInfo: this.info
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
